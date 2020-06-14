b4132a51e9d40c7bca7db1565067bc257d5203f5
Mano Sriram <mano.sriram0@gmail.com>
2020-06-15 02:40:37 +0530

    / to /share

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const server = require("./io-server.js").server;
 const io = require("./io-server.js").io;
 const cors = require("cors");
 
+app.get("/", (req, res) => {
+    res.redirect("/share");
+});
+
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.use("/share", require("./share.js"));
